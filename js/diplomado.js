a10c8ff0066afacae3100404326d85765fd35bc1
Luis Montiel <lmontiel@us_multidisc_02.intranet.ingenieria.unam.mx>
2020-02-28 15:35:32 -0600

    v2

--- js/diplomado.js
+++ js/diplomado.js
@@ -37,3 +37,20 @@ $('.material-card > .mc-btn-action').click(function () {
     }
 });
 
+$(document).ready(function(){
+ 
+    $('.ir-arriba').click(function(){
+        $('body, html').animate({
+            scrollTop: '0px'
+        }, 300);
+    });
+ 
+    $(window).scroll(function(){
+        if( $(this).scrollTop() > 0 ){
+            $('.ir-arriba').slideDown(300);
+        } else {
+            $('.ir-arriba').slideUp(300);
+        }
+    });
+ 
+});
